88214a21081b4ac7f52fc4a852f531b4bf8f68dc
saezuri510 <saezuru510@gmail.com>
2024-02-25 17:50:04 +0900

    feat: サインイン用のフォームを作成

---
+++ src/features/sign-in/components/layouts/SignInForm.tsx
@@ -0,0 +1,67 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email("メールアドレスの形式が不正です。"),
+  password: z.string().min(1, "パスワードを入力してください。"),
+});
+
+export const SignInForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8" onSubmit={(e) => void form.handleSubmit(onSubmit)(e)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input type="email" {...field} autoComplete="email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>パスワード</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} autoComplete="current-password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 import { Navbar } from "@/components/layouts/Navbar";
 import { Button } from "@/components/ui/button";
+import { SignInForm } from "@/features/sign-in/components/layouts/SignInForm";
 
 export default function SignInPage() {
   return (
     <div>
       <Navbar />
       <div>Sign in</div>
+      <SignInForm />
       <Button asChild>
         <Link href="/sign-up">Sign up</Link>
       </Button>
