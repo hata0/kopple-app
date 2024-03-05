83694034dc5fe88e56b247b5ee7532c90163cdb1
saezuri510 <saezuru510@gmail.com>
2024-03-05 16:40:34 +0900

    feat: usePortraitCarouselにロジックを移動

--- src/features/dashboard/components/layouts/PortraitCarousel.tsx
+++ src/features/dashboard/components/layouts/PortraitCarousel.tsx
@@ -1,63 +1,17 @@
-import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+import { usePortraitCarousel } from "../../hooks/usePortraitCarousel";
 import { Users } from "../../types/Users";
 
 import { PortraitCard } from "./PortraitCard";
 import { PortraitCardSkeleton } from "./PortraitCardSkeleton";
 import { PortraitMenubar } from "./PortraitMenubar";
 
-import { Carousel, CarouselApi, CarouselContent, CarouselItem } from "@/components/ui/carousel";
-import { ToastAction } from "@/components/ui/toast";
-import { toast } from "@/components/ui/use-toast";
-import { BACKEND_URL } from "@/constants/backendUrl";
-import { fetcher } from "@/utils/fetcher";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 
 export const PortraitCarousel = () => {
   const { data: users, mutate } = useSWR<Users>("/api/users");
-  const [api, setApi] = useState<CarouselApi>();
-  const [current, setCurrent] = useState(0);
-
-  useEffect(() => {
-    if (api) {
-      setCurrent(api.selectedScrollSnap());
-
-      api.on("select", () => {
-        setCurrent(api.selectedScrollSnap());
-      });
-
-      const func = async () => {
-        if (!api.canScrollNext()) {
-          const recursion = async () => {
-            const { error, res } = await fetcher(`${BACKEND_URL}/users`);
-
-            if (!error) {
-              const additionalUsers = (await res?.json()) as Users;
-              await mutate(
-                {
-                  isLikes: [...users!.isLikes, ...additionalUsers.isLikes],
-                  portraitCards: [...users!.portraitCards, ...additionalUsers.portraitCards],
-                },
-                false,
-              );
-            } else {
-              toast({
-                action: (
-                  <ToastAction altText="再取得" onClick={() => void recursion()}>
-                    再取得
-                  </ToastAction>
-                ),
-                title: "追加のデータ取得に失敗しました。",
-                variant: "destructive",
-              });
-            }
-          };
-          await recursion();
-        }
-      };
-      api.on("settle", () => void func());
-    }
-  }, [api, mutate, users]);
+  const { current, setApi } = usePortraitCarousel(users, mutate);
 
   return (
     <Carousel className="flex" orientation="vertical" setApi={setApi}>
---
+++ src/features/dashboard/hooks/usePortraitCarousel.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+// eslint-disable-next-line import/named
+import { KeyedMutator } from "swr";
+
+import { Users } from "../types/Users";
+
+import { CarouselApi } from "@/components/ui/carousel";
+import { ToastAction } from "@/components/ui/toast";
+import { toast } from "@/components/ui/use-toast";
+import { BACKEND_URL } from "@/constants/backendUrl";
+import { fetcher } from "@/utils/fetcher";
+
+export const usePortraitCarousel = (users: Users | undefined, mutate: KeyedMutator<Users>) => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (api) {
+      setCurrent(api.selectedScrollSnap());
+
+      api.on("select", () => {
+        setCurrent(api.selectedScrollSnap());
+      });
+
+      const func = async () => {
+        if (!api.canScrollNext()) {
+          const recursion = async () => {
+            const { error, res } = await fetcher(`${BACKEND_URL}/users`);
+
+            if (!error) {
+              const additionalUsers = (await res?.json()) as Users;
+              await mutate(
+                {
+                  isLikes: [...users!.isLikes, ...additionalUsers.isLikes],
+                  portraitCards: [...users!.portraitCards, ...additionalUsers.portraitCards],
+                },
+                false,
+              );
+            } else {
+              toast({
+                action: (
+                  <ToastAction altText="再取得" onClick={() => void recursion()}>
+                    再取得
+                  </ToastAction>
+                ),
+                title: "追加のデータ取得に失敗しました。",
+                variant: "destructive",
+              });
+            }
+          };
+          await recursion();
+        }
+      };
+      api.on("settle", () => void func());
+    }
+  }, [api, mutate, users]);
+
+  return {
+    current,
+    setApi,
+  };
+};
