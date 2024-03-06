deb75e8de5c5a3e5f596f790130a8248b85090b7
saezuri510 <saezuru510@gmail.com>
2024-03-06 10:08:03 +0900

    feat: talkページを作成

--- src/components/layouts/domain/PreLoginHeader.tsx
+++ src/components/layouts/domain/PreLoginHeader.tsx
@@ -1,27 +1,14 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { ReactNode } from "react";
 
 import { Button } from "../../ui/button";
 
+import { NavLinkButton } from "@/components/ui/domain/NavLinkButton";
 import { ThemeModeToggle } from "@/components/ui/domain/ThemeModeToggle";
-
-type LinkButtonProps = {
-  href: string;
-  children: ReactNode;
-};
 
 export const PreLoginHeader = () => {
   const router = useRouter();
   const pathname = router.pathname;
-
-  const LinkButton = ({ children, href }: LinkButtonProps) => {
-    return (
-      <Button asChild className={pathname === href ? "" : "text-muted-foreground"} variant="ghost">
-        <Link href={href}>{children}</Link>
-      </Button>
-    );
-  };
 
   return (
     <header aria-label="ヘッダー" className="flex">
@@ -29,9 +16,15 @@ export const PreLoginHeader = () => {
         <Link href="/">My App</Link>
       </Button>
       <nav aria-label="ナビゲーション">
-        <LinkButton href="/dashboard">ダッシュボード</LinkButton>
-        <LinkButton href="/sign-in">ログイン</LinkButton>
-        <LinkButton href="/sign-up">新規登録</LinkButton>
+        <NavLinkButton href="/dashboard" pathname={pathname}>
+          ダッシュボード
+        </NavLinkButton>
+        <NavLinkButton href="/sign-in" pathname={pathname}>
+          ログイン
+        </NavLinkButton>
+        <NavLinkButton href="/sign-up" pathname={pathname}>
+          新規登録
+        </NavLinkButton>
       </nav>
       <ThemeModeToggle />
     </header>
---
+++ src/pages/talk.tsx
@@ -0,0 +1,10 @@
+import { DashboardHeader } from "@/components/layouts/domain/DashboardHeader";
+
+export default function TalkPage() {
+  return (
+    <div>
+      <DashboardHeader />
+      <div>talk</div>
+    </div>
+  );
+}
