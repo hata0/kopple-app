dd2648dc9afcf3ec235f7b5f35dea978bbc41f85
hata0 <haataa0100@gmail.com>
2024-03-16 23:39:00 +0900

    test: バリデーションテストを実装

--- src/features/sign-in/components/layouts/SignInForm/index.test.tsx
+++ src/features/sign-in/components/layouts/SignInForm/index.test.tsx
@@ -4,15 +4,47 @@ import userEvent from "@testing-library/user-event";
 
 import { SignInForm } from ".";
 
-describe("SignInForm", () => {
-  const user = userEvent.setup();
-  describe("バリデーション", () => {
-    render(<SignInForm />);
+const user = userEvent.setup();
 
-    test("メールアドレスでないとき、エラーが表示", async () => {
-      await user.type(screen.getByRole("textbox", { name: "メールアドレス" }), "invalid value");
+describe("SignInForm", () => {
+  describe("バリデーション", () => {
+    const email = () => screen.getByRole("textbox", { name: "メールアドレス" });
+    const password = () => screen.getByLabelText("パスワード");
+    const emailError = () => screen.queryByText("メールアドレスの形式が不正です。");
+    const passwordError = () => screen.queryByText("パスワードを入力してください。");
+    const clickSubmit = async () => {
       await user.click(screen.getByRole("button", { name: "ログイン" }));
-      expect(screen.getByText("メールアドレスの形式が不正です。")).toBeInTheDocument();
+    };
+
+    describe("無効な入力のとき、該当エラーが表示される", () => {
+      it("メールアドレスの形式が不正なとき", async () => {
+        render(<SignInForm />);
+        await user.type(email(), "invalid value");
+        await clickSubmit();
+        expect(emailError()).toBeInTheDocument();
+      });
+
+      it("パスワードが入力されていないとき", async () => {
+        render(<SignInForm />);
+        await clickSubmit();
+        expect(passwordError()).toBeInTheDocument();
+      });
+    });
+
+    describe("有効な入力のとき、該当エラーは表示されない", () => {
+      it("メールアドレスであるとき", async () => {
+        render(<SignInForm />);
+        await user.type(email(), "email@example.com");
+        await clickSubmit();
+        expect(emailError()).toBeNull();
+      });
+
+      it("パスワードであるとき", async () => {
+        render(<SignInForm />);
+        await user.type(password(), "password1");
+        await clickSubmit();
+        expect(passwordError()).toBeNull();
+      });
     });
   });
 });
