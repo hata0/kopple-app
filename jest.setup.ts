c3e330d7375a1714d22878d509e9ba7b335fb0c7
hata0 <haataa0100@gmail.com>
2024-03-16 21:25:33 +0900

    test: SignInFormにメールアドレスのバリデーションテストを実装

---
+++ jest.setup.ts
@@ -0,0 +1,4 @@
+import "@testing-library/jest-dom";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock("next/router", () => require("next-router-mock"));
---
+++ src/features/sign-in/components/layouts/SignInForm/index.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+// eslint-disable-next-line import/no-named-as-default
+import userEvent from "@testing-library/user-event";
+
+import { SignInForm } from ".";
+
+describe("SignInForm", () => {
+  const user = userEvent.setup();
+  describe("バリデーション", () => {
+    render(<SignInForm />);
+
+    test("メールアドレスでないとき、エラーが表示", async () => {
+      await user.type(screen.getByRole("textbox", { name: "メールアドレス" }), "invalid@email");
+      await user.click(screen.getByRole("button", { name: "ログイン" }));
+      expect(screen.getByText("メールアドレスの形式が不正です。")).toBeInTheDocument();
+    });
+  });
+});
