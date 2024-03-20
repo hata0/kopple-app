97780f091d4d20359c1a091826ad2b5d4697124c
hata0 <haataa0100@gmail.com>
2024-03-20 18:27:39 +0900

    test: storyを作成

--- src/features/sign-in/components/layouts/SignInForm/index.stories.tsx
+++ src/features/sign-in/components/layouts/SignInForm/index.stories.tsx
@@ -1,12 +1,37 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { expect, userEvent, within } from "@storybook/test";
 
 import { SignInForm } from ".";
 
 type T = typeof SignInForm;
 type Story = StoryObj<T>;
 
+const user = userEvent.setup();
+
 export const Default: Story = {};
+
+export const EmptySubmit: Story = {
+  name: "空のまま送信した場合",
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await user.click(canvas.getByRole("button", { name: "ログイン" }));
+    await expect(await canvas.findByText("メールアドレスの形式が不正です。")).toBeInTheDocument();
+    await expect(await canvas.findByText("パスワードを入力してください。")).toBeInTheDocument();
+  },
+};
+
+export const InvalidInput: Story = {
+  name: "無効な入力の場合",
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await user.type(canvas.getByRole("textbox", { name: "メールアドレス" }), "Invalid Input");
+    await user.click(canvas.getByRole("button", { name: "ログイン" }));
+    await expect(await canvas.findByText("メールアドレスの形式が不正です。")).toBeInTheDocument();
+    await expect(await canvas.findByText("パスワードを入力してください。")).toBeInTheDocument();
+  },
+};
 
 export default {
   component: SignInForm,
+  title: "Features/SignIn/SignInForm",
 } satisfies Meta<T>;
