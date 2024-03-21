f5a77a0e2d816fef09ee4f4a908d67f2a6ddcfd4
hata0 <haataa0100@gmail.com>
2024-03-21 19:00:40 +0900

    test: signInFormに有効な入力な場合のテストを作成

--- src/features/sign-in/components/layouts/SignInForm/index.test.tsx
+++ src/features/sign-in/components/layouts/SignInForm/index.test.tsx
@@ -21,7 +21,7 @@ import * as stories from "./index.stories";
 //   });
 // });
 
-const { EmptySubmit, InvalidInput } = composeStories(stories);
+const { EmptySubmit, InvalidInput, ValidInput } = composeStories(stories);
 
 describe("SignInForm", () => {
   it("空のまま送信した場合、エラーが表示", async () => {
@@ -44,6 +44,18 @@ describe("SignInForm", () => {
       await screen.findByRole("textbox", { name: "メールアドレス" }),
     ).toHaveAccessibleDescription("メールアドレスの形式が不正です。");
     expect(await screen.findByText("パスワードを入力してください。")).toBeInTheDocument();
+  });
+
+  it("有効な入力の場合、エラーが非表示かつonSubmitが実行される", async () => {
+    const { container } = render(<ValidInput />);
+    await act(async () => {
+      await ValidInput.play?.({ canvasElement: container });
+      expect(
+        await screen.findByRole("textbox", { name: "メールアドレス" }),
+      ).not.toHaveAccessibleDescription("メールアドレスの形式が不正です。");
+      expect(screen.queryByText("パスワードを入力してください。")).not.toBeInTheDocument();
+      expect(ValidInput.args.onSubmit).toHaveBeenCalled();
+    });
   });
 
   // describe("正しい入力値でonSubmitが実行された時", () => {
