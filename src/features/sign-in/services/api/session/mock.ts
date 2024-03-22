ccd8debb238ce32845d4703ea12cf3c704cd75e4
hata0 <haataa0100@gmail.com>
2024-03-22 14:00:14 +0900

    test: httpHandlerFactoryをlibに移動

--- src/features/sign-in/services/api/session/mock.ts
+++ src/features/sign-in/services/api/session/mock.ts
@@ -2,7 +2,7 @@ import { HttpResponse } from "msw";
 
 import { path } from ".";
 
-import { httpHandlerFactory } from "@/tests/mocks/httpHandlerFactory";
+import { httpHandlerFactory } from "@/lib/mocks/httpHandlerFactory";
 
 export const getSessionHandler = httpHandlerFactory("get", path(), () =>
   HttpResponse.json({
