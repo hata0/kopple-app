413694905776db1b4a0d09e450eaa326dd4b0452
hata0 <haataa0100@gmail.com>
2024-03-23 10:10:51 +0900

    test: リンクが押された時のテストを削除

--- src/features/sign-in/components/SignInForm/index.test.tsx
+++ src/features/sign-in/components/SignInForm/index.test.tsx
@@ -1,13 +1,9 @@
 import { composeStories } from "@storybook/react";
 import { act, render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
-import mockRouter from "next-router-mock";
-import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
 
 import * as stories from "./index.stories";
 
-const { Default, EmptySubmit, InvalidInput, ValidInput } = composeStories(stories);
-const user = userEvent.setup();
+const { EmptySubmit, InvalidInput, ValidInput } = composeStories(stories);
 
 describe("SignInForm", () => {
   it("空のまま送信した場合、エラーが表示", async () => {
@@ -41,11 +37,5 @@ describe("SignInForm", () => {
     ).not.toHaveAccessibleDescription("メールアドレスの形式が不正です。");
     expect(screen.queryByText("パスワードを入力してください。")).not.toBeInTheDocument();
     expect(ValidInput.args.onSubmit).toHaveBeenCalled();
-  });
-
-  it("新規登録ページへ遷移するボタンをクリックした時、新規登録ページへ遷移している", async () => {
-    render(<Default />, { wrapper: MemoryRouterProvider });
-    await user.click(screen.getByRole("link", { name: "新規登録" }));
-    expect(mockRouter.asPath).toBe("/sign-up");
   });
 });
