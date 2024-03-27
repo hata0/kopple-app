fa0ac5cd2d07042694b1e10b7c8b5d585712a575
hata0 <haataa0100@gmail.com>
2024-03-27 22:07:09 +0900

    feat: DayOfBirthPickerを改善

--- src/features/profile/components/DayOfBirthPicker.tsx
+++ src/features/profile/components/DayOfBirthPicker.tsx
@@ -2,9 +2,9 @@ import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { ControllerRenderProps } from "react-hook-form";
 
-import { DayOfBirthCalendar } from "./DayOfBirthCalendar";
 import { FormFieldValue } from "./ProfileForm";
 
+import { DropdownCalendar } from "@/components/layouts/base/DropdownCalendar";
 import { Button } from "@/components/shadcn/ui/button";
 import { FormControl } from "@/components/shadcn/ui/form";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/shadcn/ui/popover";
@@ -22,20 +22,20 @@ export const DayOfBirthPicker = (field: ControllerRenderProps<FormFieldValue, "b
             )}
             variant="outline"
           >
-            {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+            {field.value ? format(field.value, "yyyy年MM月dd日") : <span>誕生日を選択</span>}
             <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
           </Button>
         </FormControl>
       </PopoverTrigger>
       <PopoverContent align="start" className="w-auto p-0">
-        <DayOfBirthCalendar
-          captionLayout="dropdown-buttons"
-          disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-          fromYear={1900}
+        <DropdownCalendar
+          date={{
+            from: { year: 1900 },
+            to: new Date(),
+          }}
           mode="single"
           onSelect={field.onChange}
           selected={field.value}
-          toYear={new Date().getFullYear()}
         />
       </PopoverContent>
     </Popover>
