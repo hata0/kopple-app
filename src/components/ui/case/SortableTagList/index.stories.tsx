37d5a87c37baaeff4ac9d8e91b4e95d4612f7dad
hata0 <haataa0100@gmail.com>
2024-03-29 21:11:16 +0900

    test: SortableTagListのタグを作成

--- src/components/ui/case/SortableTagList/index.stories.tsx
+++ src/components/ui/case/SortableTagList/index.stories.tsx
@@ -10,18 +10,44 @@ export const Default: Story = {};
 
 export const EmptyList: Story = {
   args: {
+    draggingTag: {
+      id: crypto.randomUUID(),
+      name: "dragging tag",
+    },
     onDeleteTag: fn(),
+    onDragEnd: fn(),
     tags: [],
   },
   name: "タグがない場合",
 };
 
+export const ManyTag: Story = {
+  args: {
+    draggingTag: {
+      id: crypto.randomUUID(),
+      name: "dragging tag",
+    },
+    onDeleteTag: fn(),
+    onDragEnd: fn(),
+    tags: Array.from({ length: 20 }).map((_, idx) => ({ id: idx.toString(), name: "tag" })),
+  },
+  name: "タグが大量にある場合",
+};
+
 export default {
   args: {
+    draggingTag: {
+      id: crypto.randomUUID(),
+      name: "dragging tag",
+    },
     onDeleteTag: fn(),
+    onDragEnd: fn(),
     tags: [
       { id: crypto.randomUUID(), name: "tag1" },
       { id: crypto.randomUUID(), name: "tag2" },
+      { id: crypto.randomUUID(), name: "tag3" },
+      { id: crypto.randomUUID(), name: "tag4" },
+      { id: crypto.randomUUID(), name: "tag5" },
     ],
   },
   component: SortableTagList,
