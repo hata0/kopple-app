00d9fa7163d30378a77267ade55c13cc2054604d
hata0 <haataa0100@gmail.com>
2024-03-29 14:12:07 +0900

    feat: Tagの見た目を改善

--- src/components/ui/case/Tag/index.tsx
+++ src/components/ui/case/Tag/index.tsx
@@ -12,11 +12,11 @@ export type TagProps = {
 
 export const Tag = ({ id, name, onDeleteTag }: TagProps) => {
   return (
-    <div className="inline-flex items-center justify-center space-x-2 rounded-md bg-primary px-2 py-1 text-primary-foreground transition-colors hover:bg-primary/90">
-      <span className="flex h-full items-center">{name}</span>
+    <div className="inline-flex items-center justify-center space-x-2 rounded-md bg-cyan-500 px-2 py-1 text-primary-foreground transition-colors hover:bg-cyan-500/90">
+      <span className="flex h-full items-center font-medium">{name}</span>
       <Button
         aria-label={`「${name}」を削除`}
-        className="ml-0.5 h-4 w-4 bg-transparent p-1 hover:bg-transparent"
+        className="ml-0.5 h-4 w-4 bg-transparent p-1 transition-colors hover:bg-transparent hover:text-red-500"
         onClick={() => onDeleteTag(id)}
         type="button"
       >
