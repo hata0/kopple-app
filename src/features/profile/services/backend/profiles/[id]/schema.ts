58d86483f3147744f2da2b962ba41193ae6202b4
hata0 <haataa0100@gmail.com>
2024-04-20 22:21:01 +0900

    feat: 画像のバリデーションを設定

--- src/features/profile/services/backend/profiles/[id]/schema.ts
+++ src/features/profile/services/backend/profiles/[id]/schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+const ACCEPTED_IMAGE_TYPES = ["image/png", "image/jpeg"];
+
 export const profileFormInputSchema = z.object({
   address: z.string(),
   age: z.coerce
@@ -18,7 +20,13 @@ export const profileFormInputSchema = z.object({
       name: z.string(),
     }),
   ),
-  image: z.instanceof(File).optional(),
+  image: z
+    .instanceof(File)
+    .optional()
+    .refine(
+      (file) => (file ? ACCEPTED_IMAGE_TYPES.includes(file.type) : true),
+      "画像である必要があります。",
+    ),
   message: z.string(),
   name: z.string().min(1, "名前を入力してください。"),
   sex: z.enum(["man", "woman"]),
