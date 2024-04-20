a075749fc175d3c6a740f0042e6511104edc7b0c
hata0 <haataa0100@gmail.com>
2024-04-20 22:35:03 +0900

    feat: imageのバリデーションを削除

--- src/features/profile/services/backend/profiles/[id]/schema.ts
+++ src/features/profile/services/backend/profiles/[id]/schema.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
-const ACCEPTED_IMAGE_TYPES = ["image/png", "image/jpeg"];
-
 export const profileFormInputSchema = z.object({
   address: z.string(),
   age: z.coerce
@@ -20,13 +18,7 @@ export const profileFormInputSchema = z.object({
       name: z.string(),
     }),
   ),
-  image: z
-    .instanceof(File)
-    .optional()
-    .refine(
-      (file) => (file ? ACCEPTED_IMAGE_TYPES.includes(file.type) : true),
-      "画像である必要があります。",
-    ),
+  image: z.instanceof(File).optional(),
   message: z.string(),
   name: z.string().min(1, "名前を入力してください。"),
   sex: z.enum(["man", "woman"]),
