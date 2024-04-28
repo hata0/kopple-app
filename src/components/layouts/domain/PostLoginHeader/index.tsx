48072c03959103961e4bff9467807990c65cd19b
hata0 <haataa0100@gmail.com>
2024-04-28 10:31:50 +0900

    feat: PostLoginHeaderのダッシュボードへのリンクを改善

--- src/components/layouts/domain/PostLoginHeader/index.tsx
+++ src/components/layouts/domain/PostLoginHeader/index.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useId } from "react";
 import { IoChatboxEllipses } from "react-icons/io5";
 
-import { DropdownHamburgerMenu } from "./DropdownHamburgerMenu";
+import { DropdownHamburgerMenu } from "../DropdownHamburgerMenu";
 
 import { Button } from "@/components/shadcn/ui/button";
 import { NavLinkButton } from "@/components/ui/domain/NavLinkButton";
 import { ThemeModeToggle } from "@/components/ui/domain/ThemeModeToggle";
 
 export const PostLoginHeader = () => {
+  const linkId = useId();
   const router = useRouter();
   const pathname = router.pathname;
 
@@ -20,10 +22,16 @@ export const PostLoginHeader = () => {
         className="flex w-full items-center justify-center space-x-3 rounded border-2 border-primary p-2 pr-8"
       >
         <div className="mr-auto flex items-center justify-center">
-          <Button asChild className="font-sans text-lg font-black" variant="ghost">
-            <Link className="flex items-center justify-center space-x-1" href="/dashboard">
+          <Button
+            asChild
+            aria-current={pathname === "/dashboard" ? "page" : undefined}
+            aria-labelledby={linkId}
+            className="flex items-center justify-center space-x-1 font-sans text-lg font-black"
+            variant="ghost"
+          >
+            <Link href="/dashboard">
               <Image alt="コップルアイコン" height={28} src="/kopple.png" width={28} />
-              <span>Kopple</span>
+              <span id={linkId}>Kopple</span>
             </Link>
           </Button>
           <NavLinkButton href="/chats" pathname={pathname}>
