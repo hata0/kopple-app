b5838b8a17a9619b860529e9dc564fdaf6e2d9de
hata0 <haataa0100@gmail.com>
2024-05-01 16:33:41 +0900

    test: テストを作成

---
+++ src/features/error/components/500/index.test.tsx
@@ -0,0 +1,26 @@
+import { composeStories } from "@storybook/react";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
+
+import * as stories from "./index.stories";
+
+const { Default } = composeStories(stories);
+
+const reloadMock = jest.spyOn(mockRouter, "reload");
+
+describe("Custom500", () => {
+  it("トップページへのリンクをクリックした時、トップページへ遷移", async () => {
+    mockRouter.setCurrentUrl("/dashboard");
+    render(<Default />, { wrapper: MemoryRouterProvider });
+    await userEvent.click(screen.getByRole("link", { name: "トップページへ" }));
+    expect(mockRouter.asPath).toBe("/");
+  });
+
+  it("リロードするをクリックした時、リロードされる", async () => {
+    render(<Default />);
+    await userEvent.click(screen.getByRole("button", { name: "リロードする" }));
+    expect(reloadMock).toHaveBeenCalled();
+  });
+});
