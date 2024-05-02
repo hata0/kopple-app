dbada642c21f91e7c3fe87daca08c61411e2cbb4
hata0 <haataa0100@gmail.com>
2024-05-02 23:43:11 +0900

    feat: jestのタイムアウトの時間を延長

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 import { randomUUID } from "node:crypto";
 
+jest.setTimeout(15000);
 // eslint-disable-next-line @typescript-eslint/no-unsafe-return
 jest.mock("next/router", () => require("next-router-mock"));
 
