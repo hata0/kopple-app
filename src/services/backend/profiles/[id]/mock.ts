d214deb53fb5ca23891f4ca3dd56e4a5f545fdab
hata0 <haataa0100@gmail.com>
2024-05-02 14:45:24 +0900

    feat: uidを変数で管理するように変更

--- src/services/backend/profiles/[id]/mock.ts
+++ src/services/backend/profiles/[id]/mock.ts
@@ -1,15 +1,17 @@
 import { HttpResponse } from "msw";
 
-import { path } from ".";
+import { path as createPath } from ".";
 
 import { httpHandlerFactory } from "@/lib/msw/httpHandlerFactory";
 import { profileContent } from "@/mocks/profileContent";
 
-export const getProfileHandler = httpHandlerFactory("get", path(":id"), () => {
+const path = createPath(":id");
+
+export const getProfileHandler = httpHandlerFactory("get", path, () => {
   return HttpResponse.json(profileContent());
 });
 
-export const postProfileHandler = httpHandlerFactory("post", path(":id"), () => {
+export const postProfileHandler = httpHandlerFactory("post", path, () => {
   return HttpResponse.json({
     message: "プロフィールを更新しました",
   });
