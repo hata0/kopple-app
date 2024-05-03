0f79e8b2ed01e82897dad5718e76566a57eef822
hata0 <haataa0100@gmail.com>
2024-05-03 11:02:16 +0900

    test: MyChatContentから表示テストを削除

--- src/features/chat/components/MyChatContent/index.test.tsx
+++ src/features/chat/components/MyChatContent/index.test.tsx
@@ -6,9 +6,8 @@ import * as stories from "./index.stories";
 const { Default } = composeStories(stories);
 
 describe("MyChatContent", () => {
-  it("時間とメッセージが表示される", () => {
+  it("Date型が時間にフォーマットされて表示される", () => {
     render(<Default />);
     expect(screen.getByText("9:32")).toBeInTheDocument();
-    expect(screen.getByText("こんにちは")).toBeInTheDocument();
   });
 });
