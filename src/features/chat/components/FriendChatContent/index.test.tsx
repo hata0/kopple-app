762a126153e5a8d8edd7e86c8ecb238e40eda16a
hata0 <haataa0100@gmail.com>
2024-05-03 11:00:55 +0900

    test: テストを作成

---
+++ src/features/chat/components/FriendChatContent/index.test.tsx
@@ -0,0 +1,13 @@
+import { composeStories } from "@storybook/react";
+import { render, screen } from "@testing-library/react";
+
+import * as stories from "./index.stories";
+
+const { Default } = composeStories(stories);
+
+describe("FriendChatContent", () => {
+  it("Date型が時間にフォーマットされて表示される", () => {
+    render(<Default />);
+    expect(screen.getByText("9:32")).toBeInTheDocument();
+  });
+});
